16c4f68a1d655359ce8861e5490ca20503880943
Ejaz Ahmed <ejaz.mohammed@utah.edu>
2022-11-29 15:16:28 -0700

    Added player movement and garbage area

---
+++ Assets/LearnMath/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace LearnMath {
+    public class PlayerMovement : MonoBehaviour {
+        [SerializeField] private Rigidbody2D body;
+        [SerializeField] private float moveSpeed = 1;
+
+        private Vector2 _moveDir = new Vector2();
+	
+        private void Awake() {
+            body = GetComponent<Rigidbody2D>();
+        }
+	
+        public void OnMove(InputAction.CallbackContext ctx) {
+            _moveDir = ctx.ReadValue<Vector2>();
+        }
+
+        private void FixedUpdate() {
+            body.velocity = _moveDir * moveSpeed;
+        }
+    }
+}
